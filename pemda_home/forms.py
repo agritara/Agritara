32c1018eeff1064defe4fb2ede80e07ed30b71b6
Arya Daniswara Khairan <arya.daniswara@ui.ac.id>
2022-10-29 15:23:21 +0700

    progress pemda-home

---
+++ pemda_home/forms.py
@@ -0,0 +1,9 @@
+# from https://www.youtube.com/watch?v=EX6Tt-ZW0so
+
+from django.forms import ModelForm
+from .models import OrderHistory
+
+class AddTaskForm(ModelForm):
+    class Meta:
+        model = OrderHistory
+        fields = ['review']
--- pemda_home/models.py
+++ pemda_home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class OrderHistory(models.Model):
+    # purchased_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    item = models.CharField(max_length=50)
+    item_amount = models.IntegerField()
+    closed_date = models.DateField(auto_now_add=True, null=True)
+    review = models.TextField()
