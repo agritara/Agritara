da8c66a38c955e468d0f9143f062cac0d1eea983
AlifioFathanHaryanto <alifio.fathan@ui.ac.id>
2022-10-30 12:11:10 +0700

    Request Pemda Alifio

--- req_item/urls.py
+++ req_item/urls.py
@@ -1,6 +1,9 @@
+from operator import add
 from django.urls import path
-from .views import index
+from req_item.views import add_governments_request
+
+app_name = 'req_item'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('request/', add_governments_request, name='add_government_request'),
 ]
--- req_item/views.py
+++ req_item/views.py
@@ -1,4 +1,75 @@
+from urllib import response
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from req_item.forms import request_form
+from req_item.models import GovReqItem
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
-def index(request):
-    return render(request, 'index.html')
+def governments_request_page(request):
+    Gov_request = GovReqItem.objects.all()
+    context = {
+        'list_request':Gov_request,
+    }
+    response = {'Gov_request':Gov_request}
+    return render(request, 'government_request_page.html', context, response)
+
+def add_governments_request(request):
+    form_request = request_form() 
+    if request.method == "POST":
+        form_request= request_form(request.POST or None, request.FILES or None)
+        if form_request.is_valid():
+            req_pemerintah = GovReqItem()
+            req_pemerintah.request = form_request.cleaned_data['request']
+            req_pemerintah.kuantitas_req = form_request.cleaned_data['kuantitas_req']
+            request = form_request.save(commit=False)
+            request.save()
+            response = HttpResponseRedirect(reverse("req_item:governments_request_page"))
+            return response
+    else:
+        print("Form tidak berhasil dibuat")
+
+    context = {'form_request':form_request,
+    'title':"Tambah Request", 
+    }
+    return render(request, 'government_request_page.html', context)
+
+def request_add(request):
+    if request.method == 'POST':
+        req = request.POST.get("request")
+        kuantitas = request.POST.get("kuantitas")
+
+        request = GovReqItem.objects.create(req="request", kuantitas="kuantitas")
+        request.save()
+        result = {
+            'fields':{
+                'request': request.request,
+                'kuantitas': request.kuantitas_req,
+            },
+            'pk': request.pk
+        }
+
+        return HttpResponse(b"CREATED", status=200)
+
+    return HttpResponseNotFound()
+
+
+    
+    
+
+
+
+
+
+
+
+
+    
+    
+
+
+
+
