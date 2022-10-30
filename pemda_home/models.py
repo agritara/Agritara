9cb97f99e6d26b998a4adca40af10f7ba61c2c6a
Arya Daniswara Khairan <arya.daniswara@ui.ac.id>
2022-10-30 10:46:01 +0700

    progress pemda-home

--- pemda_home/models.py
+++ pemda_home/models.py
@@ -1,9 +1,10 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
 class OrderHistory(models.Model):
     # purchased_by = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     item = models.CharField(max_length=50)
-    item_amount = models.IntegerField()
+    item_amount = models.IntegerField(default=0)
     closed_date = models.DateField(auto_now_add=True, null=True)
-    review = models.TextField()
+    review = models.TextField(default='')
